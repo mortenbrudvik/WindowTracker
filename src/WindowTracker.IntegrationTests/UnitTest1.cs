789bec61588bee8f50602a6f9803de751e4dfdb4
Morten Brudvik <morten@brudvik.com>
2023-01-30 19:15:38 +0100

    chore: Create projects

---
+++ src/WindowTracker.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace WindowTracker.IntegrationTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ src/WindowTracker/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace WindowTracker;
+public class Class1
+{
+
+}
